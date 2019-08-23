0d79a42f2baa7cf7f10abdf7363db7a5b96930d1
ruscat <rus.reg.36@gmail.com>
2019-08-23 10:24:29 +0300

    index.html / styling contacts

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+let photoBefore = document.querySelector('.before-after__photo-before');
+let photoAfter = document.querySelector('.before-after__photo-after');
+let buttonBefore = document.querySelector('.before-after__button--before');
+let buttonAfter = document.querySelector('.before-after__button--after');
+// console.log(photoAfter);
+buttonBefore.addEventListener('click', function (evt) {
+    photoAfter.classList.add('hidden');
+    photoBefore.classList.remove('hidden');
+});
+buttonAfter.addEventListener('click', function (evt) {
+    photoBefore.classList.add('hidden');
+    photoAfter.classList.remove('hidden');
+});
